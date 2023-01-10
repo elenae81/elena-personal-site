12ab7b734ee147af1a2a44e8afcbd0408e5ba08a
mbanerjeepalmer <mbanerjeepalmer@gmail.com>
2023-01-10 15:48:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Using
+
+- Git clone
+- Run `npm install`
+- 
+
+# How I built the site
+- Mostly relied on this tutorial: https://5balloons.info/guide-tailwindcss-eleventy-static-site/
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./_site/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'serif': ['Georgia', 'ui-serif']
+    }
+  },
+  plugins: [],
+}
